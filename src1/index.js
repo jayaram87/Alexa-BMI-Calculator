9afbee640de749205bc88b8c16bbf110774b352f
Jayaram Raja <jayaramraja1987@gmail.com>
2017-02-13 01:15:33 +0000

    analytics

--- src1/index.js
+++ src1/index.js
@@ -1,6 +1,12 @@
 var APP_ID = undefined;
 
+var express = require('express');
+var request = require('request');
 var AlexaSkill = require('./AlexaSkill');
+
+var app = express();
+
+var GA_TRACKING_ID = 'UA-YOUR-CODE-HERE';
 
 var BMICalculatorSkill = function(){
     AlexaSkill.call(this, APP_ID);
@@ -17,7 +23,7 @@ BMICalculatorSkill.prototype.eventHandlers.onSessionStarted = function (sessionS
 
 BMICalculatorSkill.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
     console.log("BMICalculatorSkill onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
-    response.tell("Welcome to the Body Mass Index calculator");
+    getWelcomeResponse(response);
 };
 
 
@@ -28,13 +34,13 @@ BMICalculatorSkill.prototype.eventHandlers.onSessionEnded = function (sessionEnd
 
 
 BMICalculatorSkill.prototype.intentHandlers = {
-    "BmiCalculatorIntent": function(intent, session, reponse){
+    "BmiCalculatorIntent": function(intent, session, response){
         handleBMIResponse(intent, session, response);
     },
     
     "AMAZON.HelpIntent": function (intent, session, response) {
         var speechText = "With BMI calculator, you can calculate your body mass index.  " +
-            "For example, if your weight is 180 lbs and height 5 ft 10 inches, your BMI is 25.8";
+            "For example, if your weight is 180 lbs and height 5 ft 10 inches, your BMI is 25.82";
         var speechOutput = {
             speech: speechText,
             type: AlexaSkill.speechOutputType.PLAIN_TEXT
@@ -48,16 +54,37 @@ BMICalculatorSkill.prototype.intentHandlers = {
             type: AlexaSkill.speechOutputType.PLAIN_TEXT
         };
         response.tell(speechOutput);
+    },
+    
+    "AMAZON.NoIntent": function (intent, session, response) {
+        trackEvent('Intent', 'AMAZON.NoIntent', 'na', '100', function(err) {
+            if (err) {
+                return next(err);
+            }
+            var speechOutput = "Okay.";
+            response.tell(speechOutput);
+        });
     }
 }
 
 function handleBMIResponse(intent, session, response){
-    var BMI = BMICalculation(intent.slots.Weight.value, intent.slots.WeightType.value, intent.slots.HeightFeet.value, intent.slots.HeightInches.value);
-    var speechOutput = {
-        speech: "According to the nhi.gov your BMU is " + BMI,
-        type: AlexaSkill.speechOutputType.PLAIN_TEXT
+    if(intent.slots.Weight.value && intent.slots.WeightType.value && intent.slots.HeightFeet.value && intent.slots.HeightInches.value){
+        var BMI = BMICalculation(intent.slots.Weight.value, intent.slots.WeightType.value, intent.slots.HeightFeet.value, intent.slots.HeightInches.value);
+        var speechOutput = {
+            speech: "According to the nhi.gov your BMI is " + BMI,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
     };
-    response.tell(speechOutput);
+    response.tell(speechOutput);} else {
+        var speechOutput = {
+            speech: "BMI Calculator requires valid weight and height to proceed ",
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        var repromptOutput = {
+            speech: "Good Bye",
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.ask(speechOutput, repromptOutput);
+    }
 }
 
 var BMICalculation = function(weight, weightType, heightFeet, heightInches){
@@ -73,7 +100,46 @@ var BMICalculation = function(weight, weightType, heightFeet, heightInches){
     var inches = Ft + inch;
 
     var BMI = ((weight * 703) / inches)/inches;
-    return BMI;
+    
+    return BMI.toFixed(2);
+}
+
+function getWelcomeResponse(response) {
+    var repromptText = "With BMI Calculator, you can calculate your body mass index. For example, if your weight is 180 lbs and height 5 ft 10 inches, your BMI is 25.8";
+    var speechText = "Welcome to BMI Calculator, please provide your weight and height. ";
+    
+    var speechOutput = {
+        speech: speechText,
+        type: AlexaSkill.speechOutputType.SSML
+    };
+    var repromptOutput = {
+        speech: repromptText,
+        type: AlexaSkill.speechOutputType.PLAIN_TEXT
+    };
+    response.ask(speechOutput, repromptOutput);
+}
+
+function trackEvent(category, action, label, value, callback) {
+    var data = {
+        v: '1', // API Version.
+        tid: GA_TRACKING_ID, // Tracking ID / Property ID.
+        // Anonymous Client Identifier. Ideally, this should be a UUID that
+        // is associated with particular user, device, or browser instance.
+        cid: '555',
+        t: 'event', // Event hit type.
+        ec: category, // Event category.
+        ea: action, // Event action.
+        el: label, // Event label.
+        ev: value, // Event value.
+    };
+
+    request.post( 'http://www.google-analytics.com/collect', { form: data }, function(err, response) {
+        if (err) { return callback(err); }
+        if (response.statusCode !== 200) {
+            return callback(new Error('Tracking failed'));
+        }
+        callback();
+    });
 }
 
 // Create the handler that responds to the Alexa Request.
